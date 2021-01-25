2c3608213ba01147a2571ef74924b3c9c288fa58
OishiShota <s92.oishi.shota@gmail.com>
2021-01-25 09:32:58 +0900

    プロトタイプ編集機能_アップデート後変換ページ修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,7 +29,7 @@ class PrototypesController < ApplicationController
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to :prototype
     else
       @prototype = Prototype.find(params[:id])
       render :edit
